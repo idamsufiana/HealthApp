5fef806cfc0640be17cf3083a35e51114a0f8b23
ida.sufiana <ida.sufiana@consultant.ioh.co.id>
2025-12-25 21:12:18 +0700

    setup

--- src/main/java/com/gits/health/HealthApp/controller/SetupController.java
+++ src/main/java/com/gits/health/HealthApp/controller/SetupController.java
@@ -13,7 +13,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 
 @RestController
-@RequestMapping({"Gits/health"})
+@RequestMapping({"api/setup"})
 @Slf4j
 public class SetupController extends BaseController {
     @Autowired
